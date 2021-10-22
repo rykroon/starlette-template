a7a9318f8e151c1513bb825ad9913101b75e67aa
Ryan Kroon <rykroon.tech@gmail.com>
2021-10-22 17:49:49 -0400

    Added redis and mongodb clients to the app state.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+from pymongo import MongoClient
+import redis
 from starlette.applications import Starlette
+from starlette.config import Config
+
 from views import routes
 from utils.exception_handlers import exception_handlers
-from utils.middleware import middleware
+from middleware import middleware
 
 
 def startup():
     ...
+
 
 app = Starlette(
     debug=False,
@@ -14,3 +19,17 @@ app = Starlette(
     routes=routes,
     on_startup=[startup]
 )
+
+config = Config()
+
+
+app.state.redis_connection_pool = redis.ConnectionPool(
+    host=config('REDIS_HOST'),
+    password=config('REDIS_PASSWORD', default=None)
+)
+
+
+app.state.mongodb_client = MongoClient(
+    config('MONGODB_HOST')
+)
+
