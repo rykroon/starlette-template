113cbc160d141e4a7087b2ebfb5d24c158c58df1
Ryan Kroon <rykroon.tech@gmail.com>
2021-11-25 12:13:16 -0500

    Removed spaces.

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,11 +2,7 @@ import os
 
 
 bind = os.getenv('GUNICORN_BIND', '0.0.0.0')
-
 loglevel = os.getenv('GUNICORN_LOGLEVEL', 'info')
-
 reload = os.getenv('GUNICORN_RELOAD', False)
-
 workers = os.getenv('GUNICORN_WORKERS', 2 * os.cpu_count() + 1)
-
 worker_class = os.getenv('GUNICORN_WORKER_CLASS', 'uvicorn.workers.UvicornWorker')
